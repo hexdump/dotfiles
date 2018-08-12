10c9b062b2b6e0852378aeacdd0ab07cf322bedb
hexdump <me@hexdump.codes>
2018-08-12 12:19:25 +0000

    Cleanup init.el

--- init.el
+++ init.el
@@ -6,49 +6,43 @@
 ;; Licensed under the terms of the GNU GPL v3.0 License.
 ;;
 
+;;
+;; remove all useless graphical cruft
+;;
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
+(menu-bar-mode -1)     ;; menubar (file, edit, etc.)
+(tool-bar-mode -1)     ;; big ugly tooltips
+(toggle-scroll-bar -1) ;; graphical scrollbar
+(set-fringe-mode -1)   ;; "fringe" borders on sides of window
 
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(toggle-scroll-bar -1)
-(set-fringe-mode -1)
-
-(load-theme 'dichromacy)
+;;
+;; don't write backups in the same directory as the file
+;;
 
 (setq backup-directory-alist `(("." . "~/.saves")))
-(setq backup-by-copying t)
 
+;;
+;; theming
+;;
+
+(load-theme 'dichromacy) ;; default theme
 (set-face-attribute 'default nil
 		    :family "Meslo"
                     :height 90
                     :weight 'normal
                     :width 'normal)
 
-;;(setq delete-old-versions t
-;;      kept-new-versions 6
-;;      kept-old-versions 2
-;;      version-control t)
+;;
+;; initialize packages
+;;
 
 (require 'package)
-
-(setq custom-file "~/.emacs.d/custom.el")
-(load custom-file 'noerror)
-
+(package-initialize)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (racket-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+;;
+;; make custom not write to init.el, but a seperate file
+;;
+
+(setq custom-file "~/.emacs.d/custom.el") ;; set custom file
+(load custom-file 'noerror)               ;; load custom file (required)
