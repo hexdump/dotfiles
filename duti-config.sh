5ba485434807ba7545cdda41f4cf79baa24c7d6d
hexdump <me@hexdump.codes>
2019-08-08 13:45:59 -0500

    add duti-config.sh

---
+++ duti-config.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+#
+# [duti-config.sh]
+#
+# automatically binds the "open with" application on all developer
+# files to your editor of choice (in my case CLI emacs).
+# Copyright (C) 2019, Liam Schumm
+#
+
+
+EDITOR_BUNDLE_ID="com.apple.automator.emacs-cli-hook"
+
+cat file-extensions.txt \
+    | grep -oE "^\.([a-z0-9]{2}|[a-z0-9]{3}|[a-z0-9]{4})$" \
+    | tr "\n" "\0" \
+    | xargs -0 -n 1 -I {} duti -s "$EDITOR_BUNDLE_ID" {} all
