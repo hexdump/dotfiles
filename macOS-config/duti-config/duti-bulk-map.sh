ac958318d2b7821f352d8e568c29519a735a23bb
hexdump <me@hexdump.codes>
2019-08-27 14:14:02 -0500

    organization

--- macOS-config/duti-config/duti-bulk-map.sh
+++ macOS-config/duti-config/duti-bulk-map.sh
@@ -8,9 +8,9 @@
 # Copyright (C) 2019, Liam Schumm
 #
 
-EDITOR_BUNDLE_ID="com.apple.automator.emacs-cli-hook"
+EDITOR_BUNDLE_ID="$1"
 
-cat file-extensions.txt \
+cat $2 \
     | grep -oE "^\.([a-z0-9]{2}|[a-z0-9]{3}|[a-z0-9]{4})$" \
     | tr "\n" "\0" \
     | xargs -0 -n 1 -I {} duti -s "$EDITOR_BUNDLE_ID" {} all
